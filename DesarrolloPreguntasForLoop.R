374e678bb3669cf058951739ba5ca1c55c840ebb
almendraqg <71927663+almendraqg@users.noreply.github.com>
2020-12-17 02:31:06 +0100

    Add files via upload

--- DesarrolloPreguntasForLoop.R
+++ DesarrolloPreguntasForLoop.R
@@ -1,28 +1,29 @@
 
 library("dplyr");
+library("ggplot2");
 
 # RN1
 # Cargamos las bases de datos
 
 # Micro
-micro_chile <- read.csv2("C:/Users/PC/Desktop/chileContacto/micro_chile.csv")
-micro_colombia <- read.csv2("C:/Users/PC/Desktop/chileContacto/micro_colombia.csv")
-micro_peru <- read.csv2("C:/Users/PC/Desktop/chileContacto/micro_peru.csv")
+micro_chile <- read.csv2("micro_chile.csv")
+micro_colombia <- read.csv2("micro_colombia.csv")
+micro_peru <- read.csv2("micro_peru.csv")
 
 # Pequeña
-pequena_chile <- read.csv2("C:/Users/PC/Desktop/chileContacto/pequena_chile.csv")
-pequena_colombia <- read.csv2("C:/Users/PC/Desktop/chileContacto/pequena_colombia.csv")
-pequena_peru <- read.csv2("C:/Users/PC/Desktop/chileContacto/pequena_peru.csv")
+pequena_chile <- read.csv2("pequena_chile.csv")
+pequena_colombia <- read.csv2("pequena_colombia.csv")
+pequena_peru <- read.csv2("pequena_peru.csv")
 
 # Mediana 
-medianas_chile <- read.csv2("C:/Users/PC/Desktop/chileContacto/medianas_chile.csv")
-medianas_colombia <- read.csv2("C:/Users/PC/Desktop/chileContacto/medianas_colombia.csv")
-medianas_peru <- read.csv2("C:/Users/PC/Desktop/chileContacto/medianas_peru.csv")
+medianas_chile <- read.csv2("medianas_chile.csv")
+medianas_colombia <- read.csv2("medianas_colombia.csv")
+medianas_peru <- read.csv2("medianas_peru.csv")
 
 # Grande
-grandes_chile <- read.csv2("C:/Users/PC/Desktop/chileContacto/grandes_chile.csv")
-grandes_colombia <- read.csv2("C:/Users/PC/Desktop/chileContacto/grandes_colombia.csv")
-grandes_peru <- read.csv2("C:/Users/PC/Desktop/chileContacto/grandes_peru.csv")
+grandes_chile <- read.csv2("grandes_chile.csv")
+grandes_colombia <- read.csv2("grandes_colombia.csv")
+grandes_peru <- read.csv2("grandes_peru.csv")
 
 
 # Se crean los vectores de la variable Tamanio
@@ -138,20 +139,64 @@ if(peru_ingresos >= chile_ingresos && peru_ingresos >= colombia_ingresos) {
 # Se agrega la variable Colombia y se divide entre 10
 
 nueva_variable_CL <- chile$tasa_interes*0.1
-names(nueva_variable_CL)[0] = "nueva_variable"
+nueva_variable_CL <- as.data.frame(nueva_variable_CL)
+names(nueva_variable_CL)[1] = "nueva_variable"
 
 nueva_variable_PE <- peru$tasa_interes+0.3
+nueva_variable_PE <- as.data.frame(nueva_variable_PE)
 names(nueva_variable_PE)[1] = "nueva_variable"
 
 nueva_variable_CO <- colombia$tasa_interes/10
+nueva_variable_CO <- as.data.frame(nueva_variable_CO)
 names(nueva_variable_CO)[1] = "nueva_variable"
   
-data <- rbind(micro_chile, pequena_chile, medianas_chile, micro_colombia, grandes_chile,
-              micro_peru, pequena_peru, medianas_peru, grandes_peru,
-              pequena_colombia, medianas_colombia, grandes_colombia)
+nueva_variable <- rbind(nueva_variable_CL,
+              nueva_variable_PE,
+              nueva_variable_CO)
+              
+data <- cbind(data, nueva_variable)           
+     
+
+# Gráfico del porcentaje de participación de mujeres que trabajan en la empresa
+
+ggplot(data, aes(x=fecha, y=porcentaje_mujeres, group = pais, colour =pais )) + 
+  ggtitle("Porcentaje de participación de mujeres que trabajan en la empresa") +
+  geom_line()  + 
+  xlab("FECHA")+
+  ylab("% de participación de mujeres que trabajan en la empresa")
+  geom_point( size=2, shape=21, fill="white") + 
+  theme_minimal()         
+        
+# Monto en millones de dólares de cuanto importan estas empresas
 
 
+ggplot(data, aes(x=fecha, y=importaciones, group = pais, colour =pais )) + 
+  ggtitle("Monto en millones de dólares de cuanto importan estas empresas") +
+  geom_line()  + 
+  xlab("FECHA")+
+  ylab("Millones de dólares")
+  geom_point( size=2, shape=21, fill="white") + 
+  theme_minimal()
 
 
+# Gráfico de ingresos para cada país durante los periodos de muestra
 
 
+ggplot(data, aes(x=fecha, y=ingresos, group = pais, colour =pais )) + 
+  ggtitle("Ingresos de explotación de las empresas") +
+  geom_line()  + 
+  xlab("FECHA")+
+  ylab("INGRESOS")
+  geom_point( size=2, shape=21, fill="white") + 
+  theme_minimal()
+    
+    
+# Gráfico de reservas para cada país durante los periodos de muestra  
+    
+ggplot(data, aes(x=fecha, y=reservas, group = pais, colour =pais )) + 
+  ggtitle("Activos que mantienen sin movimiento en caso de dificultades") +
+  geom_line()  + 
+  xlab("FECHA")+
+  ylab("RESERVAS")
+  geom_point( size=2, shape=21, fill="white") + 
+  theme_minimal()
